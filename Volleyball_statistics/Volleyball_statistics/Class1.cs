548985701f1d3a6dc70ccbf8e9d63aef31bc3f16
preislet <tomas.preisler1@gmail.com>
2022-06-18 10:48:10 +0200

    Trida menu

---
+++ Volleyball_statistics/Volleyball_statistics/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volleyball_statistics
+{
+    internal class Menu
+    {
+        string Domaci;
+        string Hoste;
+        int Sestava;
+
+        public Menu(string domaci, string hoste, int sestava)
+        {
+            Domaci = domaci;
+            Hoste = hoste;
+            Sestava = sestava;
+        }   
+    }
+}
