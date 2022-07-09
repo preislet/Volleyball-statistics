f72d4fffdbdb8809a6a547f0de3de39012fb0f2a
preislet <tomas.preisler1@gmail.com>
2022-07-09 20:16:12 +0200

    Abstract class Hrac

--- Volleyball_statistics/Volleyball_statistics/Class1.cs
+++ Volleyball_statistics/Volleyball_statistics/Class1.cs
@@ -26,7 +26,7 @@ namespace Volleyball_statistics
             Sestava = null;
         }
 
-        //Public classes
+        //Public functins
         public string FindPath(string s)
         {
             //new použito při tvoření nové složky na Desktopu
@@ -83,6 +83,7 @@ namespace Volleyball_statistics
             {
                 try
                 {
+                    //Vytvoření hlavní složky
                     System.IO.Directory.CreateDirectory(path);
                 }
                 catch(Exception e)
@@ -91,6 +92,7 @@ namespace Volleyball_statistics
                 }
                 try
                 {
+                    //Vytvoření podsložky Sestavy
                     System.IO.Directory.CreateDirectory(path2);
                 }
                 catch (Exception e)
@@ -99,6 +101,7 @@ namespace Volleyball_statistics
                 }
                 try
                 {
+                    //Vytvoření podsložky Tabulky Excel
                     System.IO.Directory.CreateDirectory(path3);
                 }
                 catch (Exception e)
@@ -117,7 +120,7 @@ namespace Volleyball_statistics
         public int PocetBoduHoste = 0;
         public bool LastBod;  //true-Domácí, false-Hosté
 
-        //Public classes
+        //Public functions
         public void ProcentaDopadu(int X, int Y, PictureBox p)
         {
             //Vypocet pozice na obrázku je vypočítaný jako pozice kliku - x a y souřadnice obrázku hřiště (rozměry 630x318)
@@ -197,6 +200,75 @@ namespace Volleyball_statistics
     }
     public class Postaveni_a_StatistikaHracu
     {
+        abstract class Hrac
+        {
+            // index 0 - za 1, index 1 - za 3; index 2 - za 5; index 3 - chyba (pole ma pouze 1 nebo chyba)
+            // Chyba je hodnocena známkou 0
+            protected int[] servis = new int[4];
+            protected int[] blok = new int[4];
+            protected int[] pole = new int[2];
+            protected int[] utok = new int[4];
+
+            // Public functions
+
+            /// <summary>
+            /// Zajišťování zápisu statistiky servisu hráče (int z je známka servisu)
+            /// 1 - eso
+            /// 3 - soupeřící tým nepřijal za 1
+            /// 5 - soupeřící tým přijal za 1
+            /// chyba - chyba
+            /// </summary>
+            protected void Servis(int z) { }
+
+
+            /// <summary>
+            /// Zajišťování zápisu statistiky bloku hráče (int z je známka bloku)
+            /// 1 - bodový blok
+            /// 3 - soupeřící tým po bloku ve velikých problémech
+            /// 5 -soupeřící tým zvedl v pohodě balón
+            /// chyba - chyba bloku
+            /// </summary>
+            protected void Blok(int z) { }
+
+
+            /// <summary>
+            /// Zajišťování zápisu statistiky pole hráče (int z je známka pole)
+            /// 1 - výborný zákrok
+            /// chyba - kritická (nevynucená chyba v poli)
+            /// </summary>
+            protected void Pole(int z) { }
+
+
+            /// <summary>
+            /// Zajišťování zápisu statistiky útoku hráče (int z je známka útoku)
+            /// 1 - bodový útok
+            /// 3 - soupeřící tým ve velkých problémech
+            /// 5 - soupeřící tým přijal balón bez problému
+            /// chyba - chyba na útoku (out, blok)
+            /// </summary>
+            protected void Utok(int z) { }
+        }
+        class Smecar: Hrac 
+        {
+            string Pozice = "Smecar";
+        }
+        class Blokar: Hrac 
+        {
+            string Pozice = "Smecar";
+        }
+        class Nahravac: Hrac 
+        { 
+
+        }
+        class Univerzal: Hrac 
+        { 
+
+        }
+        class Libero: Hrac 
+        { 
+        
+        }
+        public int[] HraciDomaci;
         public int[] PostaveniDomac;
         public int[] PostaveniHoste;
 
