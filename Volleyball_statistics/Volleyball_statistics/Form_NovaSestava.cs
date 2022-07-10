2f971e04d8e4899c339438ce2f231b376dc2dacb
preislet <tomas.preisler1@gmail.com>
2022-07-10 19:58:20 +0200

    messageBox pro upozornění duplicitného jména sestavy

--- Volleyball_statistics/Volleyball_statistics/Form_NovaSestava.cs
+++ Volleyball_statistics/Volleyball_statistics/Form_NovaSestava.cs
@@ -19,6 +19,11 @@ namespace Volleyball_statistics
 
         private void button_Ulozit_Click(object sender, EventArgs e)  //Ukložení nové sestavy do složky vytvořené na ploše
         {
+            if (KontrolaDuplicity())
+            {
+                Varovani();
+                return;
+            }
             string path = Form1.menu.FindPath(textBox43.Text);  //textBox43 je kolonka, kam se vyplňuje název sestavy
             Form1.menu.SaveNewTeam(path, tableLayoutPanel1);
             this.Close();
@@ -30,5 +35,35 @@ namespace Volleyball_statistics
 
             // TODO - kontorla, zda se už takhle nějaká sestava nejmenuje
         }
+
+
+        /// <summary>
+        /// Funkce kontroluje, zda se ve složce nachází sestava se stehným jménem či nikoliv
+        /// </summary>
+        private bool KontrolaDuplicity()
+        {
+            string jmeno = textBox43.Text;
+            List<string> jmenaSouboru = new List<string>();
+            string path = Form1.menu.FindPath("sestavy");
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] Files = dir.GetFiles("*.txt"); //Nalezení všexh .txt files ve složce
+            for (int i = 0; i < Files.Length; i++)
+            {
+                if(Files[i].Name.Remove(Files[i].Name.Length - 4) == textBox43.Text) return true;
+            }
+            return false; 
+        }
+
+
+        /// <summary>
+        /// Funkce spoušrící MassageBox
+        /// </summary>
+        private void Varovani() 
+        {
+            string message = "Sestava s tímto jménem již existuje. Zvolte jiné jméno";
+            MessageBox.Show(message);
+        }
+
+
     }
 }
